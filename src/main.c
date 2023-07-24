b5a738f71f2e27be1aa75629c4f8efdfc6681788
Viktor Diezel <viktor.diezel@posteo.de>
2023-07-24 22:20:17 +0200

    dinner time

--- src/main.c
+++ src/main.c
@@ -4,19 +4,12 @@
 
 int CAP_FRAME_RATE = TRUE;
 
-int game_is_running = FALSE;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
+int game_is_running = FALSE;
 int last_frame_time = 0;
-
-struct ball {
-  float x;
-  float y;
-  float width;
-  float height;
-} ball;
-
+int field[FIELD_ROWS][FIELD_COLUMNS];
 
 int initialize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -49,10 +42,22 @@ int initialize_window(void) {
 }
 
 void setup() {
-  ball.x = 20;
-  ball.y = 20;
-  ball.width = 15;
-  ball.height = 15;
+  for (int i = 0; i < FIELD_ROWS; i++) {
+    for (int j = 0; j < FIELD_COLUMNS; j++) {
+      field[i][j] = 0;
+    }
+  }
+}
+
+void handle_key_down(SDL_Event event) {
+  switch (event.key.keysym.sym) {
+    case SDLK_ESCAPE:
+      game_is_running = FALSE;
+      break;
+    case SDLK_1:
+      field[19][0] = 1;
+      break;
+  }
 }
 
 void process_inputs() {
@@ -64,8 +69,8 @@ void process_inputs() {
       game_is_running = FALSE;
       break;
     case SDL_KEYDOWN:
-      if (event.key.keysym.sym == SDLK_ESCAPE)
-        game_is_running = FALSE;
+      handle_key_down(event);
+      break;
   }
 }
 
@@ -83,26 +88,47 @@ void update() {
     cap_frame_rate();
   }
 
-  float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
+  //float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
   last_frame_time = SDL_GetTicks();
+}
 
-  ball.x += 70 * delta_time;
-  ball.y += 70 * delta_time;
+void renderField() {
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+  SDL_Rect field_rect = {
+    FIELD_ORIGIN_X - 1,
+    FIELD_ORIGIN_Y - 1,
+    FIELD_WIDTH + 2,
+    FIELD_HEIGHT + 2,
+  };
+
+  SDL_RenderDrawRect(renderer, &field_rect);
+
+  for (int i = 0; i < FIELD_ROWS; i++) {
+    for (int j = 0; j < FIELD_COLUMNS; j++) {
+      int has_block = field[i][j];
+
+      if (has_block) {
+        SDL_Rect block = {
+          FIELD_ORIGIN_X + i * BLOCK_WIDTH,
+          FIELD_ORIGIN_Y + j * BLOCK_WIDTH,
+          BLOCK_WIDTH,
+          BLOCK_WIDTH,
+        };
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderFillRect(renderer, &block);
+      }
+    }
+  }
+
 }
 
 void render() {
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
 
-  SDL_Rect ball_rect = {
-    (int) ball.x,
-    (int) ball.y,
-    (int) ball.width,
-    (int) ball.height
-  };
+  renderField();
 
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  SDL_RenderFillRect(renderer, &ball_rect);
   SDL_RenderPresent(renderer);
 }
 
