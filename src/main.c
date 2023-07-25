0638e7d77cd2a149b8b42f8f02a5d5602488f6f7
Viktor Diezel <viktor.diezel@posteo.de>
2023-07-25 11:04:21 +0200

    added T piece to play around

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+// Tetris game using the Original Rotation System
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include "./constants.h"
@@ -8,8 +9,35 @@ SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
 int game_is_running = FALSE;
+int has_piece = FALSE;
 int last_frame_time = 0;
 int field[FIELD_ROWS][FIELD_COLUMNS];
+
+int curr_piece[4][2] = {{-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}};
+int last_gravity = 0;
+
+int PIECE_T[4][3][3] = {
+  {
+    {0, 0, 0},
+    {1, 1, 1},
+    {0, 1, 0},
+  },
+  {
+    {0, 1, 0},
+    {0, 1, 1},
+    {0, 1, 0},
+  },
+  {
+    {0, 1, 0},
+    {1, 1, 1},
+    {0, 0, 0},
+  },
+  {
+    {0, 1, 0},
+    {1, 1, 0},
+    {0, 1, 0},
+  },
+};
 
 int initialize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -49,13 +77,48 @@ void setup() {
   }
 }
 
+int valid_move(int row_dir, int col_dir) {
+  for (int i = 0; i < 4; i++) {
+    int row_idx = curr_piece[i][0];
+    if (row_dir == 1 && row_idx >= (FIELD_ROWS - 1)) return FALSE;
+
+    int col_idx = curr_piece[i][1];
+    if (col_dir == 1 && col_idx >= (FIELD_COLUMNS - 1)) return FALSE;
+    if (col_dir == -1 && col_idx <= 0) return FALSE;
+  }
+
+  return TRUE;
+}
+
+void moveLateral(int dir) {
+  if (has_piece && valid_move(0, dir)) {
+    for (int i = 0; i < 4; i++) {
+      curr_piece[i][1] = curr_piece[i][1] + dir;
+    }
+  }
+}
+
+void soft_drop() {
+  if (has_piece && valid_move(-1, 0)) {
+    for (int i = 0; i < 4; i++) {
+      curr_piece[i][0] = curr_piece[i][0] + 1;
+    }
+  }
+}
+
 void handle_key_down(SDL_Event event) {
   switch (event.key.keysym.sym) {
     case SDLK_ESCAPE:
       game_is_running = FALSE;
       break;
-    case SDLK_1:
-      field[19][0] = 1;
+    case SDLK_a:
+      moveLateral(-1);
+      break;
+    case SDLK_d:
+      moveLateral(1);
+      break;
+    case SDLK_s:
+      soft_drop();
       break;
   }
 }
@@ -74,6 +137,62 @@ void process_inputs() {
   }
 }
 
+void spawn_piece() {
+  int tile_counter = 0;
+
+  for (int m = 0; m < 3; m++) {
+    for (int n = 0; n < 3; n++) {
+      int field_block = field[m][n + 4];
+      int piece_block = PIECE_T[0][m][n];
+
+      if (piece_block && field_block) {
+        // game over
+        return;
+      }
+
+      if (piece_block) {
+        curr_piece[tile_counter][0] = m;
+        curr_piece[tile_counter][1] = n + 4;
+        tile_counter++;
+      }
+    }
+  }
+
+  has_piece = TRUE;
+}
+
+void merge_piece() {
+  for (int i = 0; i < 4; i++) {
+    int row_idx = curr_piece[i][0];
+    int col_idx = curr_piece[i][1];
+    field[row_idx][col_idx] = 1;
+  }
+}
+
+void reset_current_piece() {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 2; j++) {
+      curr_piece[i][j] = -1;
+    }
+  }
+  has_piece = FALSE;
+}
+
+void apply_gravity() {
+  if (!has_piece) return;
+
+  if (valid_move(1, 0)) {
+    for (int i = 0; i < 4; i++) {
+      curr_piece[i][0] = curr_piece[i][0] + 1;
+    }
+  } else {
+    merge_piece();
+    reset_current_piece();
+  }
+
+  last_gravity = SDL_GetTicks();
+}
+
 void cap_frame_rate() {
   int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - last_frame_time);
 
@@ -88,8 +207,24 @@ void update() {
     cap_frame_rate();
   }
 
+  if (!has_piece) {
+    spawn_piece();
+  } else if ((SDL_GetTicks() - last_gravity) > 500) {
+    apply_gravity();
+  }
+
   //float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
   last_frame_time = SDL_GetTicks();
+}
+
+void renderFieldBlock(int row, int col) {
+  SDL_Rect block = {
+    FIELD_ORIGIN_X + col * BLOCK_WIDTH,
+    FIELD_ORIGIN_Y + row  * BLOCK_WIDTH,
+    BLOCK_WIDTH,
+    BLOCK_WIDTH,
+  };
+  SDL_RenderFillRect(renderer, &block);
 }
 
 void renderField() {
@@ -109,18 +244,20 @@ void renderField() {
       int has_block = field[i][j];
 
       if (has_block) {
-        SDL_Rect block = {
-          FIELD_ORIGIN_X + i * BLOCK_WIDTH,
-          FIELD_ORIGIN_Y + j * BLOCK_WIDTH,
-          BLOCK_WIDTH,
-          BLOCK_WIDTH,
-        };
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderFillRect(renderer, &block);
+        renderFieldBlock(i, j);
       }
     }
   }
 
+  if (has_piece) {
+    for (int i = 0; i < 4; i++) {
+      int *tile = curr_piece[i];
+      int row_idx = tile[0];
+      int col_idx = tile[1];
+
+      renderFieldBlock(row_idx, col_idx);
+    }
+  }
 }
 
 void render() {
